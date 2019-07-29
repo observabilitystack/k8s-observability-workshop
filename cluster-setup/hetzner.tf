fe5714c6ccf41e223ebf2c7d6a42ace50182427d
Torsten Bøgh Köster <tbk@thiswayup.de>
2019-07-29 10:45:51 +0200

    make things multi-server ready

--- cluster-setup/hetzner.tf
+++ cluster-setup/hetzner.tf
@@ -1,6 +1,6 @@
 
 variable "instance_count" {
-  default = "1"
+  default = "2"
 }
 
 # Configure the Hetzner Cloud Provider using
@@ -47,13 +47,15 @@ resource "hcloud_ssh_key" "default" {
 
 # We use random pet names to name each workshop
 # server: numbers are boooooring
-resource "random_pet" "server" {}
+resource "random_pet" "server" {
+  count       = "${var.instance_count}"
+}
 
 # This creates the workshop servers. Increase count
 # to the number of participants
 resource "hcloud_server" "workshop" {
   count       = "${var.instance_count}"
-  name        = "${random_pet.server.id}"
+  name        = "${element(random_pet.server.*.id, count.index)}"
   image       = "centos-7"
   server_type = "cx41"   # 4cpu
   ssh_keys    = ["${hcloud_ssh_key.default.name}"]
@@ -62,20 +64,22 @@ resource "hcloud_server" "workshop" {
 # This creates a DNS record as a subdomain of
 # k8s.012stack.org that points to the host
 resource "digitalocean_record" "hostname" {
+  count  = "${var.instance_count}"
   domain = "k8s.o12stack.org"
   type   = "A"
-  name   = "${hcloud_server.workshop.name}"
-  value  = "${hcloud_server.workshop.ipv4_address}"
+  name   = "${element(hcloud_server.workshop.*.name, count.index)}"
+  value  = "${element(hcloud_server.workshop.*.ipv4_address, count.index)}"
 }
 
 # This creates a wildcard subdomain pointing to
 # the hostname. We use this for different services
 # on our workshop servers later
 resource "digitalocean_record" "wildcard" {
+  count  = "${var.instance_count}"
   domain = "k8s.o12stack.org"
   type   = "CNAME"
-  name   = "*.${hcloud_server.workshop.name}"
-  value  = "${hcloud_server.workshop.name}.k8s.o12stack.org."
+  name   = "*.${element(hcloud_server.workshop.*.name, count.index)}"
+  value  = "${element(hcloud_server.workshop.*.name, count.index)}.k8s.o12stack.org."
 }
 
 # Create a private key to request TLS certs with
@@ -91,9 +95,10 @@ resource "acme_registration" "o12stack" {
 
 # Request wildcard TLS cert
 resource "acme_certificate" "certificate" {
+  count                     = "${var.instance_count}"
   account_key_pem           = "${acme_registration.o12stack.account_key_pem}"
-  common_name               = "*.${hcloud_server.workshop.name}.k8s.o12stack.org"
-  subject_alternative_names = ["${hcloud_server.workshop.name}.k8s.o12stack.org"]
+  common_name               = "*.${element(hcloud_server.workshop.*.name, count.index)}.k8s.o12stack.org"
+  subject_alternative_names = ["${element(hcloud_server.workshop.*.name, count.index)}.k8s.o12stack.org"]
 
   dns_challenge {
     provider = "digitalocean"
@@ -102,6 +107,7 @@ resource "acme_certificate" "certificate" {
 
 # write TLS certs into Ansible
 resource "local_file" "certificates" {
-    content     = "${acme_certificate.certificate.certificate_pem}${acme_certificate.certificate.issuer_pem}${acme_certificate.certificate.private_key_pem}"
-    filename = "./roles/boostrap/tls/files/${hcloud_server.workshop.name}.k8s.o12stack.org.pem"
+    count    = "${var.instance_count}"
+    content  = "${element(acme_certificate.certificate.*.certificate_pem, count.index)}${element(acme_certificate.certificate.*.issuer_pem, count.index)}${element(acme_certificate.certificate.*.private_key_pem, count.index)}"
+    filename = "./roles/bootstrap/tls/files/${element(hcloud_server.workshop.*.name, count.index)}.k8s.o12stack.org.pem"
 }
