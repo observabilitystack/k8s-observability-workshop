6da8e23d5706065ce078f71fd63b57b52609ee41
Torsten Bøgh Köster <tbk@thiswayup.de>
2019-07-31 15:38:08 +0200

    arrrg

--- k8s/meta/kube-resource-report/deploy.sh
+++ k8s/meta/kube-resource-report/deploy.sh
@@ -8,8 +8,8 @@ export KUBE_REPORT_VERSION=v2.11.1
 export HOSTNAME="${HOSTNAME:=local}"
 
 echo "🚀  ${application} ${KUBE_REPORT_VERSION}"
-kubectl -n monitoring apply -f service.yml
-kubectl -n monitoring apply -f rbac.yml
-envsubst <ingress.yml | kubectl apply -n monitoring  -f -
-envsubst <deployment.yml | kubectl apply -n monitoring  -f -
+kubectl -n monitoring apply -f service.yaml
+kubectl -n monitoring apply -f rbac.yaml
+envsubst <ingress.yaml | kubectl apply -n monitoring  -f -
+envsubst <deployment.yaml | kubectl apply -n monitoring  -f -
 echo -e "🙌  Done.\n"
