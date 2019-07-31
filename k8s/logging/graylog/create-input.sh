a6077e175f7a93884525f4b499059e7a72f2319a
Torsten Bøgh Köster <tbk@thiswayup.de>
2019-07-31 11:47:35 +0200

    add gl input creation script

---
+++ k8s/logging/graylog/create-input.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export HOSTNAME="${HOSTNAME:=local}"
+
+set -e
+
+echo "🔌  Configuring Graylog TCP input"
+curl -sS --request POST \
+  --url http://local.o12stack.org/api/system/inputs \
+  --header 'authorization: Basic YWRtaW46bzEyc3RhY2s=' \
+  --header 'content-type: application/json' \
+  --header 'x-requested-by: k8s-observability-workshop' \
+  --data '{
+  "title": "gelf-tcp",
+  "type": "org.graylog2.inputs.gelf.tcp.GELFTCPInput",
+  "configuration": {
+    "bind_address": "0.0.0.0",
+    "port": 12201
+  },
+  "global": true
+}'
+echo -e "🙌  Done.\n"
