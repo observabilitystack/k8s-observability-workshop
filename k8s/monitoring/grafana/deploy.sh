b7d313aaf56e43a71b67e5a7375690029e984951
Torsten Bøgh Köster <tbk@thiswayup.de>
2019-08-01 12:53:58 +0200

    add grafana

---
+++ k8s/monitoring/grafana/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+set -e
+
+application=$(basename $(pwd))
+export GRAFANA_VERSION=6.2.5
+export HOSTNAME="${HOSTNAME:=local}"
+export CONF_SHA=$(shasum ${application}-configuration.yml | awk '{print $1}')
+export ENV_SHA=$(shasum ${application}-environment.yml | awk '{print $1}')
+
+echo "🚀  ${application} ${GRAFANA_VERSION}"
+envsubst <${application}-configuration.yml | kubectl apply -n monitoring  -f -
+envsubst <${application}-environment.yml | kubectl apply -n monitoring  -f -
+kubectl -n monitoring apply -f ${application}-service.yml
+envsubst <${application}-ingress.yml | kubectl apply -n monitoring  -f -
+envsubst <${application}-deployment.yml | kubectl apply -n monitoring  -f -
+echo -e "🙌  Done.\n"
