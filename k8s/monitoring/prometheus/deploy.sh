61698c8ad5f6f0b7839c7da7685880e73de2fbfd
Torsten Bøgh Köster <tbk@thiswayup.de>
2019-08-01 16:00:12 +0200

    test relabeling

--- k8s/monitoring/prometheus/deploy.sh
+++ k8s/monitoring/prometheus/deploy.sh
@@ -9,7 +9,7 @@ export HOSTNAME="${HOSTNAME:=local}"
 export CONF_SHA=$(shasum ${application}-configuration.yml | awk '{print $1}')
 
 echo "🚀  ${application} ${PROMETHEUS_VERSION}"
-kubectl -n monitoring apply -f ${application}-configuration.yml
+envsubst <${application}-configuration.yml | kubectl apply -n monitoring  -f -
 kubectl -n monitoring apply -f ${application}-service.yml
 kubectl -n monitoring apply -f ${application}-rbac.yml
 envsubst <${application}-ingress.yml | kubectl apply -n monitoring  -f -
