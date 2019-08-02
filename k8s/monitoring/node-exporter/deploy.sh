681a6560f8056af229b82c8bad5318b5068357e1
Torsten Bøgh Köster <tbk@thiswayup.de>
2019-08-02 13:42:01 +0200

    fix node exporter deployment

--- k8s/monitoring/node-exporter/deploy.sh
+++ k8s/monitoring/node-exporter/deploy.sh
@@ -7,6 +7,5 @@ export NODE_EXPORTER_VERSION=v0.18.1
 application=$(basename $(pwd))
 
 echo "🚀  ${application} ${NODE_EXPORTER_VERSION}"
-kubectl -n monitoring apply -f ${application}-service.yml
 envsubst <${application}-daemonset.yml | kubectl apply -n monitoring  -f -
 echo -e "🙌  Done.\n"
