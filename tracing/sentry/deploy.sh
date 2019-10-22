921b2b18f71eb45eb87b551b2828b6f6bd244853
Torsten Bøgh Köster <tbk@thiswayup.de>
2019-10-22 20:06:35 +0200

    [grafana] more automation

--- tracing/sentry/deploy.sh
+++ tracing/sentry/deploy.sh
@@ -6,10 +6,10 @@ set -e
 application=$(basename $(pwd))
 export HOSTNAME="${HOSTNAME:=local}"
 export DOMAIN_NAME="${DOMAIN_NAME:=k8s.o12stack.org}"
-export ENV_SHA=$(shasum ${application}-environment.yml | awk '{print $1}')
+export ENV_SHA=$(shasum ../sentry-infrastructure/${application}-environment.yml | awk '{print $1}')
+export CONF_SHA=$(shasum ${application}-configuration.yml | awk '{print $1}')
 
 echo "🚀  ${application}"
-envsubst <${application}-environment.yml | kubectl apply -n tracing  -f -
 kubectl -n tracing apply -f ${application}-service.yml
 envsubst <${application}-ingress.yml | kubectl apply -n tracing  -f -
 envsubst <${application}-deployment.yml | kubectl apply -n tracing  -f -
