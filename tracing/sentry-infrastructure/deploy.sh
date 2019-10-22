e68bb647f1479988728fca94ed5e8b15392176b1
Torsten Bøgh Köster <tbk@thiswayup.de>
2019-10-22 20:18:54 +0200

    [sentry] fix denkfehler

--- tracing/sentry-infrastructure/deploy.sh
+++ tracing/sentry-infrastructure/deploy.sh
@@ -4,10 +4,8 @@ cd $(dirname "$0")
 set -e
 
 application=$(basename $(pwd))
-export ENV_SHA=$(shasum sentry-environment.yml | awk '{print $1}')
 
 echo "🚀  ${application}"
-envsubst <sentry-environment.yml | kubectl apply -n tracing  -f -
 envsubst <${application}-deployment.yml | kubectl apply -n tracing  -f -
 kubectl -n tracing apply -f ${application}-service.yml
 echo -e "🙌  Done.\n"
--- tracing/sentry/deploy.sh
+++ tracing/sentry/deploy.sh
@@ -6,12 +6,13 @@ set -e
 application=$(basename $(pwd))
 export HOSTNAME="${HOSTNAME:=local}"
 export DOMAIN_NAME="${DOMAIN_NAME:=k8s.o12stack.org}"
-export ENV_SHA=$(shasum ../sentry-infrastructure/${application}-environment.yml | awk '{print $1}')
+export ENV_SHA=$(shasum ${application}-environment.yml | awk '{print $1}')
 export CONF_SHA=$(shasum ${application}-configuration.yml | awk '{print $1}')
 
 echo "🚀  ${application}"
 kubectl -n tracing apply -f ${application}-service.yml
 envsubst <${application}-configuration.yml | kubectl apply -n tracing  -f -
+envsubst <${application}-environment.yml | kubectl apply -n tracing  -f -
 envsubst <${application}-ingress.yml | kubectl apply -n tracing  -f -
 envsubst <${application}-deployment.yml | kubectl apply -n tracing  -f -
 echo -e "🙌  Done.\n"
