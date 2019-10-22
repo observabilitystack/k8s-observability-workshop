bba49c9432c4fd123d0d3d80455d2a73ea8db208
Torsten Bøgh Köster <tbk@thiswayup.de>
2019-10-22 16:43:52 +0200

    [sentry] fix this and that

--- tracing/sentry/deploy.sh
+++ tracing/sentry/deploy.sh
@@ -9,8 +9,8 @@ export DOMAIN_NAME="${DOMAIN_NAME:=k8s.o12stack.org}"
 export ENV_SHA=$(shasum ${application}-environment.yml | awk '{print $1}')
 
 echo "🚀  ${application}"
-envsubst <${application}-environment.yml | kubectl apply -n monitoring  -f -
-kubectl -n monitoring apply -f ${application}-service.yml
-envsubst <${application}-ingress.yml | kubectl apply -n monitoring  -f -
-envsubst <${application}-deployment.yml | kubectl apply -n monitoring  -f -
+envsubst <${application}-environment.yml | kubectl apply -n tracing  -f -
+kubectl -n tracing apply -f ${application}-service.yml
+envsubst <${application}-ingress.yml | kubectl apply -n tracing  -f -
+envsubst <${application}-deployment.yml | kubectl apply -n tracing  -f -
 echo -e "🙌  Done.\n"
