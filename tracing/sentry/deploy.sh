75c753d323048c8864e475ce16e266f14561b4db
Torsten Bøgh Köster <tbk@thiswayup.de>
2019-10-22 20:17:02 +0200

    [sentry] fix other deployment

--- tracing/sentry/deploy.sh
+++ tracing/sentry/deploy.sh
@@ -11,6 +11,7 @@ export CONF_SHA=$(shasum ${application}-configuration.yml | awk '{print $1}')
 
 echo "🚀  ${application}"
 kubectl -n tracing apply -f ${application}-service.yml
+envsubst <${application}-configuration.yml | kubectl apply -n tracing  -f -
 envsubst <${application}-ingress.yml | kubectl apply -n tracing  -f -
 envsubst <${application}-deployment.yml | kubectl apply -n tracing  -f -
 echo -e "🙌  Done.\n"
