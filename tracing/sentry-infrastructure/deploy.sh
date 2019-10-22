e40841a5a31e39e3ddfa14001aca25e5664da57d
Torsten Bøgh Köster <tbk@thiswayup.de>
2019-10-22 20:15:50 +0200

    [sentry] fix deployment

--- tracing/sentry-infrastructure/deploy.sh
+++ tracing/sentry-infrastructure/deploy.sh
@@ -4,10 +4,10 @@ cd $(dirname "$0")
 set -e
 
 application=$(basename $(pwd))
-export ENV_SHA=$(shasum ${application}-environment.yml | awk '{print $1}')
+export ENV_SHA=$(shasum sentry-environment.yml | awk '{print $1}')
 
 echo "🚀  ${application}"
-envsubst <${application}-environment.yml | kubectl apply -n tracing  -f -
+envsubst <sentry-environment.yml | kubectl apply -n tracing  -f -
 envsubst <${application}-deployment.yml | kubectl apply -n tracing  -f -
 kubectl -n tracing apply -f ${application}-service.yml
 echo -e "🙌  Done.\n"
