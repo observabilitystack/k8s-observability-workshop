fd74ddc7804ef78a3e8f24aae551ddcb834f1a08
Torsten Bøgh Köster <tbk@thiswayup.de>
2019-10-23 21:32:10 +0200

    [elastic-apm] remove uneeded hash

--- tracing/elastic-apm/deploy.sh
+++ tracing/elastic-apm/deploy.sh
@@ -7,6 +7,7 @@ application=$(basename $(pwd))
 export ELASTIC_VERSION=7.4.0
 export HOSTNAME="${HOSTNAME:=local}"
 export DOMAIN_NAME="${DOMAIN_NAME:=k8s.o12stack.org}"
+export CONF_SHA=$(shasum ${application}-configuration.yml | awk '{print $1}')
 
 echo "🚀  ${application} ${ELASTIC_VERSION}"
 kubectl -n tracing apply -f ${application}-service.yml
